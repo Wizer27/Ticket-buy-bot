82013e3bc3d8199cc8f6f3dd9fb57f612b2f9f64
Wizer27 <svd2yar@gmail.com>
2025-11-19 14:27:06 +0300

    fix

--- main.py
+++ main.py
@@ -10,15 +10,43 @@ import uvicorn
 import uuid
 
 
+
+
 path = "data/secrets.json"
+
+
+
 
 def get_api_key() -> str:
     try:
         with open(path,"r") as file:
             data = json.load(file)
-        return data["key"]    
+        return data["api"]    
     except Exception as e:
         raise KeyError(f"Error : {e}")
+
+def get_siganture_key() -> str:
+    try:
+        with open(path,"r") as file:
+            data = json.load(file)
+        return data["signature"]    
+    except Exception as e:
+        raise KeyError("No key found")
+
+
+def verify_signature(data: dict, received_signature: str) -> bool:
+    if time.time() - data.get('timestamp', 0) > 300:
+        return False
+    KEY = get_siganture_key()
+    
+    data_to_verify = data.copy()
+    data_to_verify.pop("signature", None)
+    
+    data_str = json.dumps(data_to_verify, sort_keys=True, separators=(',', ':'))
+    expected_signature = hmac.new(KEY.encode(), data_str.encode(), hashlib.sha256).hexdigest()
+    
+    return hmac.compare_digest(received_signature, expected_signature)
+
 
 async def safe_get(req:Request):
     api = req.headers.get("X-API-KEY")
@@ -32,4 +60,8 @@ async def main():
     return "Buyer BOT API"
 
 
-
+class Register(BaseModel):
+    user_id:str
+@app.get("/register")
+async def register(req:Register,x_signature:str = Header(...),x_timestamp:str = Header(...)):
+    if not 
